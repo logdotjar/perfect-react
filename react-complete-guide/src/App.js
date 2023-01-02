4d9a473ca7cf45c42535138c530179be9dc25754
jinareum <jinareum.76@gmail.com>
2023-01-02 20:37:55 +0900

    환경설정 및 파일별 용도 추가

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Lets Get started!
+    </div>
+  );
+}
+
+export default App;
