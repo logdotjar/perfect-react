9ffd7cdfb0797b82b8b819f56920be127511327f
jinareum <jinareum.76@gmail.com>
2023-01-03 19:39:37 +0900

    JSX 방식의 컴포넌트 작성

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,10 +1,10 @@
-// import logo from './logo.svg';
-// import './App.css';
+import ExpenseItem  from "./components/ExpenseItem";
+
 
 function App() {
   return (
     <div className="App">
-     Lets Get started!
+        <ExpenseItem></ExpenseItem>
     </div>
   );
 }
