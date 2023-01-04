95343a06e9a81c1289f1e2b1c7cb0aace8c43280
jinareum <jinareum.76@gmail.com>
2023-01-05 01:55:22 +0900

    JSX 동적데이터 출력및 표현식

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -4,12 +4,20 @@ import './ExpenseItem.css';
 
 
 function ExpenseItem(){
+
+    const expenseDate = new Date(2023,1,5);
+    const expenseTitle = "Car insurance";
+    const expenseAmount = 293.45;
+
     return (
         <div className="expense-item">
-            <div>2023.01.04</div>
+            <div>{expenseDate.toISOString()}</div>
+            {/*date객체이기때문에 expenseDate로 바로 출력불가능*/}
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$234.64</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">
+                    {expenseAmount}
+                </div>
             </div>
         </div>
     );
