bc150978b0b8c9b80dce09c6d8762f592faa2671
jinareum <jinareum.76@gmail.com>
2023-01-04 18:46:23 +0900

    JSX 방식의 CSS 스타일 추가

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,18 @@
+// 특정 파일이 있다고 알려야함 - CSS파일을 고려할수있도
+import './ExpenseItem.css';
+
+
+
 function ExpenseItem(){
-    return <h2> expense item!</h2>
+    return (
+        <div className="expense-item">
+            <div>2023.01.04</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$234.64</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
