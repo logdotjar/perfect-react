8b4203be508620f06234a12d3b13bc3b36f8da0d
jinareum <jinareum.76@gmail.com>
2023-01-06 04:39:56 +0900

    비용 컴포넌트 추가

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem  from "./components/ExpenseItem";
+import Expense  from "./components/Expense";
 
 
 function App() {
@@ -31,26 +31,8 @@ function App() {
 
   return (
     <div className="App">
-        <ExpenseItem
-            title={expense[0].title}
-            amount={expense[0].amount}
-            date={expense[0].date}/>
-
-        <ExpenseItem
-            title={expense[1].title}
-            amount={expense[1].amount}
-            date={expense[1].date}/>
-
-        <ExpenseItem
-            title={expense[2].title}
-            amount={expense[2].amount}
-            date={expense[2].date}/>
-
-        <ExpenseItem
-            title={expense[3].title}
-            amount={expense[3].amount}
-            date={expense[3].date}/>
-
+        <div className="expense"></div>
+        <Expense items={expense}/>
     </div>
   );
 }
