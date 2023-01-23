0e6e9db9d4d08a2d60f690a2b821b3d474a48b4e
jinareum <jinareum.76@gmail.com>
2023-01-23 23:08:21 +0900

    fix: 동적CSS 설정

--- styled-component-react/src/components/CourseGoals/CourseInput/CourseInput.js
+++ styled-component-react/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,19 +5,28 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.length > 0){
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length === 0){
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
+  // JS -> 백틱문자에는 문자열에 동적인값을 넣을 수있다.
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
