1fa19a274895a34c7ca153dbb6c58ab7b038add6
jinareum <jinareum.76@gmail.com>
2023-01-26 07:15:34 +0900

    refactor : CSS Module로 코드 변경

--- styled-component-react/src/components/UI/Button/Button.js
+++ styled-component-react/src/components/UI/Button/Button.js
@@ -1,7 +1,12 @@
-import styled from 'styled-components';
+import React from 'react';
+
+import styles from './Button.module.css';
+// import styled from 'styled-components';
 
 //javascript tagged template literals
-const Button = styled.button`
+/*
+const Button = styled.button` 
+    width: 100%;
     font: inherit;
     padding: 0.5rem 1.5rem;
     border: 1px solid #8b005d;
@@ -9,6 +14,10 @@ const Button = styled.button`
     background: #8b005d;
     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
     cursor: pointer;
+  
+  @media (min-width: 768px){
+    width: auto;
+  }
 
   &:focus {
     outline: none;
@@ -21,15 +30,15 @@ const Button = styled.button`
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
 `;
+ */
 
-/*
+
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
 };
- */
 
 export default Button;
